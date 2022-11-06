6e01f8dd1939dff498fcada8dee85bdc0d17fefc
tuan0101 <tuan.programmer0101@gmail.com>
2022-11-06 14:28:51 -0800

    CreatePlayerHealth (Refactor from PlayerController)

---
+++ Assets/_Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,33 @@
+using System;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    int myHP = 100;
+    public HealthBar healthBar;
+    int damageAmount = 25;
+    public Action OnDie = delegate { };
+
+    // getting attack from the enemies
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "projectile")
+        {
+            TakeDamage(damageAmount);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        myHP -= damage;
+        if (myHP <= 0)
+            Die();
+    }
+
+    void Die()
+    {
+        myHP = 0;
+        healthBar.health = myHP / 100f;
+        OnDie();
+    }
+}
