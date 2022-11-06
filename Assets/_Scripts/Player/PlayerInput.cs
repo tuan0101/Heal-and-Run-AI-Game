1eaf4d6acf843b990b9272e5a93c1c24ee093096
tuan0101 <tuan.programmer0101@gmail.com>
2022-11-06 14:08:54 -0800

    Create PlayerInput.cs (Single Responsibility)

--- Assets/_Scripts/Player/PlayerInput.cs
+++ Assets/_Scripts/Player/PlayerInput.cs
@@ -1,18 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PlayerInput : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float Horizontal { get; private set; }
+    public float Vertical { get; private set; }
+    public float Sing { get; private set; }
+
+    public event Action OnSing = delegate { };
 
     // Update is called once per frame
     void Update()
     {
-        
+        Horizontal = Input.GetAxisRaw("Horizontal");
+        Vertical = Input.GetAxisRaw("Vertical");
+        Sing = Input.GetAxisRaw("Interact");
+        if (Sing != 0) OnSing();
     }
 }
