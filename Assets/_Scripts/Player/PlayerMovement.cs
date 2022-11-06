68b88f06dea89739cb382d4560c9116deb30ea40
tuan0101 <tuan.programmer0101@gmail.com>
2022-11-06 14:16:42 -0800

    create PlayerMovement.cs (single responsibility)

--- Assets/_Scripts/Player/PlayerMovement.cs
+++ Assets/_Scripts/Player/PlayerMovement.cs
@@ -22,46 +22,10 @@ public class PlayerMovement : MonoBehaviour
         anim = obj.GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AnimateSing()
     {
-        
-    }
-
-    private void sing2()
-    {
-        if (Input.GetAxisRaw("Interact") != 0)
-        //if (Input.GetKeyDown(KeyCode.F))
-        {
-            if (singing == false)
-            {
-                audioSource.Play();
-                // Singing Animation
-                anim.SetBool("isSing", true);
-            }
-            singRange.SetActive(true);
-            singing = true;
-            ParticalManager.SetActive(true);
-        }
-        else
-        {
-            audioSource.clip = songs[Random.Range(0, songs.Length)];
-            audioSource.Stop();
-            singing = false;
-            singRange.SetActive(false);
-            ParticalManager.SetActive(false);
-            // Do nothing
-            anim.SetBool("isSing", false);
-        }
-    }
-
-    void Sing()
-    {
-        if (singing == false)
-        {
-            // Singing Animation
-            anim.SetBool("isSing", true);
-        }
+        // Singing Animation
+        anim.SetBool("isSing", true);
     }
 
 }
