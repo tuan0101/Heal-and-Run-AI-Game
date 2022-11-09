067f405cc3a952344d5d7fdd8b2a13e1c97f12a0
tuan0101 <tuan.programmer0101@gmail.com>
2022-11-09 14:50:54 -0800

    Create Terrain.cs

---
+++ Assets/_Scripts/Enemy/Terrain.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Terrain : MonoBehaviour
+{
+    [SerializeField] GameObject terrain;
+    MeshCollider col;
+
+
+    float minX, maxX, minZ, maxZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        col = terrain.GetComponent<MeshCollider>();
+
+        // Note: cannot call col.bounds in update()
+        minX = col.bounds.min.x;
+        maxX = col.bounds.max.x;
+        minZ = col.bounds.min.z;
+        maxZ = col.bounds.max.z;
+    }
+
+    // return random position within the terrain
+    public Vector3 ClampPosition(Vector3 pos)
+    {
+        return new Vector3(Mathf.Clamp(pos.x, minX, maxX), pos.y, Mathf.Clamp(pos.z, minZ, maxZ));
+    }
+}
